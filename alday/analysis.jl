5afeaa04750c6352913898b6fd56b3b17cab0541
Phillip Alday <me@phillipalday.com>
2023-09-13 03:32:45 -0500

    kb07

--- alday/analysis.jl
+++ alday/analysis.jl
@@ -14,3 +14,12 @@ bc1 = fit(BoxCoxTransformation, fm1)
 fm2 = fit(MixedModel,
           @formula(1000 / reaction ~ 1 + days + (1 + days|subj)),
           dataset(:sleepstudy))
+
+fm3 = fit(MixedModel,
+          @formula(rt_raw ~ 1 + spkr * prec * load + 
+                            (1 + prec | item) + 
+                            (1 | subj)),
+          dataset(:kb07))
+
+bc3 = fit(BoxCoxTransformation, fm3)
+boxcoxplot(bc3; conf_level=0.95)
